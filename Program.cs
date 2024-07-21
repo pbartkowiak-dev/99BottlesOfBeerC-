ae28ff4f2bbcdced22081a43e3aafafd8cb5c849
pbartkowiak-dev <bartkop@wz.hasbro.com>
2024-07-21 16:25:31 +0200

    The App + tests

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Bottles;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var bottles = new Bottles();
+        var song = bottles.Song();
+
+        Console.WriteLine(song);
+    }
+}
